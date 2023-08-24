596d5a135e3cad714186009a7e6db1b3a4bd25b1
Burak <99614754+burakkertn@users.noreply.github.com>
2023-08-24 13:53:04 +0300

    Add project files.

---
+++ Selenium_Project/Controllers/RegisterController.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_Project.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult StartAutomation()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://example.com/register");
+
+            IWebElement usernameInput = driver.FindElement(By.Id("username"));
+            IWebElement emailInput = driver.FindElement(By.Id("email"));
+            IWebElement passwordInput = driver.FindElement(By.Id("password"));
+            IWebElement registerButton = driver.FindElement(By.Id("register-button"));
+
+            usernameInput.SendKeys("burakkertn");
+            emailInput.SendKeys("burakkertnn@gmail.com");
+            passwordInput.SendKeys("123456Aa*");
+
+            registerButton.Click();
+
+            // Sayfanın yüklenmesini beklemek için bekleyin (WebDriverWait kullanmanız önerilir)
+
+            driver.Quit();
+
+            return View("Index");
+        }
+    }
+}
